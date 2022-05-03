4d41aa6a89a94dd9a504713c3d4a0233a8eef89c
Luca Goettle <l-goettle@gmx.de>
2022-05-03 19:24:00 +0200

    wip read and parse osm

---
+++ Assets/Scripts/OSM_Generator.cs
@@ -0,0 +1,46 @@
+using UnityEditor;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using OsmSharp.Streams;
+using OsmSharp.Complete;
+
+public class MenuTest : MonoBehaviour
+{
+
+    // Add a menu item named "Do Something" to MyMenu in the menu bar.
+    [MenuItem("OSM/Generate Map from OSM data")]
+    static void MenuItem()
+    {
+        Debug.Log("Checking if map data is available");
+        GenerateMapData(@".\Assets\OSM\test-map.osm");
+        Debug.Log("Read the data");
+    }
+
+    private static void GenerateMapData(string filePath)
+    {
+        using (var fileStream = new FileInfo(filePath).OpenRead())
+        {
+            XmlOsmStreamSource source = new XmlOsmStreamSource(fileStream);
+
+            // Get all building ways and nodes 
+            var tmp_ways = from osmGeo in source
+                            where osmGeo.Type == OsmSharp.OsmGeoType.Node ||
+                            (osmGeo.Type == OsmSharp.OsmGeoType.Way)
+                            select osmGeo;
+
+            // Should filter before calling ToComplete() to reduce memory usage
+            var completes = tmp_ways.ToComplete(); // Create Complete objects (for Ways gives them a list of Node objects)
+            var ways = from osmGeo in completes
+                       where osmGeo.Type == OsmSharp.OsmGeoType.Way
+                       select osmGeo;
+            CompleteWay[] completeWays = ways.Cast<CompleteWay>().ToArray();
+            Debug.Log("Parsed osm file");
+            Debug.Log("There are " + completeWays.Length + " ways");
+        }
+    }
+}
+
